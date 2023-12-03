6fbf50f12c9b42e5b3b25f72a45a32fa37a6b7cd
Bethan Williams <bethanw10@gmail.com>
2023-12-03 21:42:07 +0000

    Day 3 part 2

--- Day03/Day03.cs
+++ Day03/Day03.cs
@@ -2,27 +2,27 @@
 
 internal class Day03
 {
+    internal record Point(int X, int Y);
+
+    private static readonly Point[] Delta = {
+        new(1, 0),  new(1, 1),  new(1, -1),
+        new(0, 1),              new(0, -1),
+        new(-1, 1), new(-1, 0), new(-1, -1),
+    };
+
     private static void Main()
     {
         var input = File.ReadAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "input.txt"));
 
         var lines = input.Split("\r\n");
 
-        Part1(lines);
-        Part2(lines);
+        Console.WriteLine(Part1(lines));
+        Console.WriteLine(Part2(lines));
     }
 
-    public static void Part1(string[] lines)
+    public static string Part1(string[] lines)
     {
         var total = 0;
-
-        var delta = new[]
-        {
-            new Point(1, 0),  new Point(1, 1),  new Point(1, -1),
-            new Point(0, 1),                    new Point(0, -1),
-            new Point(-1, 1), new Point(-1, 0), new Point(-1, -1),
-        };
-
         for (var y = 0; y < lines.Length; y++)
         {
             var line = lines[y];
@@ -38,10 +38,9 @@ internal class Day03
 
                     if (!foundAdjacentSymbol)
                     {
-                        foundAdjacentSymbol = delta
+                        foundAdjacentSymbol = Delta
                             .Select(d => new Point(d.X + x, d.Y + y))
-                            .Where(p => p.Y >= 0 && p.Y < lines.Length &&
-                                        p.X >= 0 && p.X < lines[p.Y].Length)
+                            .Where(p => IsInBounds(lines, p))
                             .Select(p => lines[p.Y][p.X])
                             .Any(ch => ch != '.' && !char.IsDigit(ch));
                     }
@@ -64,28 +63,81 @@ internal class Day03
             }
         }
 
-        Console.WriteLine(total);
+        return total.ToString();
     }
 
-    public static void Part2(string[] lines)
+    public static string Part2(string[] lines)
     {
-       
+        var allAsterisks = new Dictionary<Point, List<int>>();
+
+        for (var y = 0; y < lines.Length; y++)
+        {
+            var line = lines[y];
+            var number = "";
+            var asterisks = new HashSet<Point>();
+
+            for (var x = 0; x < line.Length; x++)
+            {
+                var c = line[x];
+                if (char.IsDigit(c))
+                {
+                    number += c;
+
+                    var adjacentAsterisks = Delta
+                        .Select(d => new Point(d.X + x, d.Y + y))
+                        .Where(p => IsInBounds(lines, p) && lines[p.Y][p.X] == '*')
+                        .ToArray();
+                    
+                    foreach (var asterisk in adjacentAsterisks)
+                    {
+                        asterisks.Add(asterisk);
+                    }
+                }
+                else
+                {
+                    AddAsterisks(number, asterisks, allAsterisks);
+
+                    number = "";
+                    asterisks = new HashSet<Point>();
+                }
+            }
+
+            AddAsterisks(number, asterisks, allAsterisks);
+        }
+
+        var total = allAsterisks
+            .Where(a => a.Value.Count == 2)
+            .Select(a => a.Value.Aggregate(1, (n1, n2) => n1 * n2))
+            .Sum();
+
+        return total.ToString();
     }
 
-  
-}
-
-internal class SchematicNumber
-{
-    public string Num = "";
-    public int X1;
-    public int X2;
-    public int Y;
-
-    public override string ToString()
+    private static void AddAsterisks(string number, HashSet<Point> adjacentAsterisks, Dictionary<Point, List<int>> gears)
     {
-        return $"{Num} x: {X1} - {X2} y: {Y}";
+        if (!adjacentAsterisks.Any())
+        {
+            return;
+        }
+
+        var parsedNum = int.Parse(number);
+
+        foreach (var asterisk in adjacentAsterisks)
+        {
+            if (gears.TryGetValue(asterisk, out var numbers))
+            {
+                numbers.Add(parsedNum);
+            }
+            else
+            {
+                gears.Add(asterisk, new List<int> { parsedNum });
+            }
+        }
+    }
+
+    private static bool IsInBounds(string[] lines, Point p)
+    {
+        return p.Y >= 0 && p.Y < lines.Length &&
+               p.X >= 0 && p.X < lines[p.Y].Length;
     }
 }
-
-internal record Point(int X, int Y);
